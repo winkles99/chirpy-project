682d15f7dca7c9fe9cb714733479f851b4028d53
winkles99 <kiah.hurt@outlook.com>
2025-08-22 20:52:31 -0500

    Clean commit: only Chirpy project files

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/winkles99/chirpy-project
+
+go 1.24.2
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+	github.com/winkles99/chirpy v0.0.0-20250823004317-38fc76eea3ac
+)
---
+++ reset.go
@@ -0,0 +1,21 @@
+package main
+
+import "net/http"
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Reset is only allowed in dev environment."))
+		return
+	}
+
+	cfg.fileserverHits.Store(0)
+	err := cfg.db.Reset(r.Context())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Failed to reset the database: " + err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0 and database reset to initial state."))
+}
